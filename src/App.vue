bb8c3ae75be01a64752d9818700741ce859cb8f0
“seven” <252519042@qq.com>
2023-05-31 16:16:14 +0800

    提交格式化后的代码

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h1>我是app</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '我愛你祖國'
+console.log(str)
+</script>
+
+<style scoped lang="scss">
+.app {
+  width: 200px;
+  height: 100px;
+  background: black;
+  h1 {
+    color: red;
+  }
+}
+</style>

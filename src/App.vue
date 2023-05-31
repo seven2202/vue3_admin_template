57c61fb19ed89ceefe4e379d2463b0dadf977cd1
“seven” <252519042@qq.com>
2023-05-31 16:49:50 +0800

    feat: add h2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="app">
     <h1>我是app</h1>
+    <h2>新增了一个commitlint</h2>
   </div>
 </template>
 
 <script setup lang="ts">
 const str = '我愛你祖國'
 console.log(str)
+const fun = () => {
+  console.log('這是沒有格式化的代碼')
+}
+
+fun()
 </script>
 
 <style scoped lang="scss">
